8a546f28c29ac5cbff2010eab79708b8fce0fcdb
Paulo <paulo@facti.local>
2018-06-20 15:05:16 -0300

    Incluindo serviço para testes.

---
+++ src/main/java/com/paulosalgado/Controller.java
@@ -0,0 +1,14 @@
+package com.paulosalgado;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Admin Client!";
+    }
+
+}
