38a5dd5d3a0acff21af6d73807ccc74d7f1c1443
Paulo Salgado <pjosalgado@gmail.com>
2018-08-11 16:21:40 -0300

    Incluindo serviço de teste.

--- src/main/java/com/paulosalgado/Controller.java
+++ src/main/java/com/paulosalgado/Controller.java
@@ -1,14 +1,26 @@
 package com.paulosalgado;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
+	
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Admin Client!";
+        return "Hello from Admin Client! I'm running on " + getMachineInfo();
     }
-
+    
+    private String getMachineInfo() {
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			return inetAddress.getHostAddress() + " / " + inetAddress.getHostName();
+		} catch (UnknownHostException e) {
+			return "#error";
+		}
+    }
+    
 }
